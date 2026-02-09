e5725166a0973cd41f20558ed4537411091c1b06
Nicole <nrenn306@mtroyal.ca>
2026-02-09 12:44:02 -0700

    finished implementing the SQL for each API route using SQL editor in supabase... now slowly converting

--- music-server.js
+++ music-server.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require('express'); //
 const supa = require('@supabase/supabase-js');
 const app = express();
 
@@ -7,27 +7,42 @@ const supaAnonKey = 'sb_publishable_nLmIszWZN079y3wq1Hzp7A_NB5F7PuB';
 
 const supabase = supa.createClient(supaUrl, supaAnonKey);
  
+//first route to test connection to supabase
+//server will listen for requests at http://localhost:8080/blahblahblah
+
+const domain = 'http://localhost:8080/';
+//const domain = 'https:vercke or render';
+const musicAPI = domain + 'music/artists';
+
 app.get('/music/artists', async (req, res) => {
     const { data, error } = await supabase
     .from('artists')
-    .select(`artist_id, artist_name, artist_image_url, spotify_url, spotify_desc, types!inner(type_name)`)
-    .order('artist_name', { ascending: true });
-    if(error) {
-        console.log('1');
+    .select('*')
+    .order('artist_name');
+    if (error) {
+        console.error('Error fetching data:', error);
+        return;
     }
     res.json(data);
 });
 
-
 app.get('/api/artists/:artistID', async (req, res) => {
     const { data, error } = await supabase
+    .from('artists')
+    .select('artist_name')
+    .eq('artist_id', req.params.artistID);
+    //still produced a [] when artistID did not exist
+    //only did an error when :artistID
     if(error) {
         console.log('2');
     }
   res.send(data);
 });
-app.get('/api/artists/averages/ref ', async (req, res) => {
+app.get('/api/artists/averages/:artistID', async (req, res) => {
     const { data, error } = await supabase
+      .from('songs')
+      .select('AVERGAE(bpm), AVERAGE(energy), AVERAGE(danceability), AVERAGE(loudness), AVERAGE(liveness), AVERAGE(valence), AVERAGE(duration), AVERAGE(acousticness), AVERAGE(speechineess), AVERAGE(popularity)')
+      .eq('artist_id', req.params.artistID);
     if(error) {
         console.log('3');
     }
@@ -90,8 +105,10 @@ app.get('/api/mood/studying/ref', async (req, res) => {
   res.send(data);
 });
 
-
-app.listen(8080, () => {
+// this is where the server listens for requests
+app.listen(8080, () => { 
 console.log('listening on port 8080');
-console.log('http://localhost:8080/music/artists');
+console.log(musicAPI); //https:localhost:8080/music/artists
 });
+
+//refer to https://www.geeksforgeeks.org/web-tech/express-js-req-params-property/
